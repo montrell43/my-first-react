83d21284023a606aa10520b7f10080d5283e0715
montrell43 <tmerriweather@persevernow.org>
2025-08-12 13:40:55 -0500

    add commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div style={{backgroundColor: "lightblue", padding: "1rem", color: "white",textAlign: "center", display: "flex", justifyContent: "center"
-    }}><h1>Taurus</h1></div>
+    }}><h1>Taurus M. Merriweathr</h1></div>
   )
 }
 
