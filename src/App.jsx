1631425b64ee3839143b7d438c6daac622e335fc
montrell43 <tmerriweather@persevernow.org>
2025-08-12 13:40:18 -0500

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import Body from "./components/Body"
 import Footer from "./components/Footer"
 
-function App(props) {
+function App() {
   return (
     <div>
         <Header />
